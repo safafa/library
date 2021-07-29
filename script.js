38d3cc65e2917b7161433a2d349806cee2e41d94
safafa <safaaballagh97@gmail.com>
2021-07-29 15:46:27 +0100

    Add function to display books

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let myLibrary = [];
+const table = document.getElementById('books');
 
-function Book(title,author,pages) {
+function Book(title, author, pages) {
   // the constructor...
   this.title = title;
   this.author = author;
@@ -9,5 +10,27 @@ function Book(title,author,pages) {
 
 function addBookToLibrary(book) {
   // do stuff here
-  myLibrary.push(book)
+  myLibrary.push(book);
+}
+
+book1 = new Book("test", "tester", 10);
+book2 = new Book("test", "tester", 10);
+book3 = new Book("test", "tester", 10);
+book4 = new Book("test", "tester", 10);
+
+addBookToLibrary(book1)
+addBookToLibrary(book2)
+addBookToLibrary(book3)
+addBookToLibrary(book4)
+
+function displayBooks() {
+    myLibrary.map((book)=> {
+        row = table.insertRow();
+        title = row.insertCell();
+        author = row.insertCell();
+        pages = row.insertCell();
+        title.innerHTML = book.title;
+        author.innerHTML = book.author;
+        pages.innerHTML = book.pages;
+    })
 }
